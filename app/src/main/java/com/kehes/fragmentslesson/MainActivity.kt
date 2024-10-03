5a917e87d3215a66fdf68d28d08d64f6e3677701
Vampirish <vamperish@gmail.com>
2024-10-03 18:33:20 +0200

    tied up MainFragment to MainActivity

--- app/src/main/java/com/kehes/fragmentslesson/MainActivity.kt
+++ app/src/main/java/com/kehes/fragmentslesson/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import com.kehes.fragmentslesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        supportFragmentManager
+        supportFragmentManager.commit {
+            replace(R.id.container, MainFragment())
+        }
     }
 }
